df40130614151eb0e03ffc4819e6c0405547a06c
me <me@andrewbruce.net>
2008-03-16 02:51:48 +0000

    removed lib/server.rb and tidied controller.rb

--- lib/controller.rb
+++ lib/controller.rb
@@ -15,22 +15,18 @@ class Controller
     controller_name = self.class.to_s.gsub(/Controller$/, '').underscore
     template_filename = "#{action_name}.#{content_type}.xq"
 
-    File.join(Server.root, 'app', 'views', controller_name, template_filename)
+    File.join(Server.root, 'app', 'views', controller_name, 
+              template_filename)
   end
 
   def render
-    
     begin
       query_source = File.read template_path(:atomserv)
     rescue Errno::ENOENT
       query_source = File.read template_path(:atom)
     end
 
-    options = {
-      :variables => {:path => path}
-    }
-
-    Xml.query query_source, options
+    Xml.query query_source, :variables => {:path => path}
   end
 
 end
